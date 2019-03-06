9160a42007bc923e3402d40a3071ce0334c28d56
redocyzal <achuth.i@gmail.com>
2019-03-06 13:21:31 +1100

    algorithms - Coins

--- src/main/java/com/aiyyatti/algorithms/misc/recursionanddynamic/KnapSack.java
+++ src/main/java/com/aiyyatti/algorithms/misc/recursionanddynamic/KnapSack.java
@@ -70,7 +70,7 @@ public class KnapSack {
                 localSumOfSeries = findBestValueWithoutMemonization(i + 1, newWeightTillNow, localSumOfSeries + value[i], value, weight, W, seriesString);
             }
             maxValue = Math.max(localSumOfSeries, maxValue);
-            logger.debug("Series: {} Permissible Weight: {} Best Total(in order): {}", seriesString, W, maxValue);
+            //logger.debug("Series: {} Permissible Weight: {} Best Total(in order): {}", seriesString, W, maxValue);
         }
         return maxValue;
     }
@@ -114,7 +114,7 @@ public class KnapSack {
         int[] value = new int[]{1, 4, 2, 5, 7, 9, 0, 2, 1, 3, 6, 8, 0, 0, -2, 1, 3, 1, 2, 7, 9, 0, 7, 5, 4, 3, 3, 5, 6, 7, 8, 9, 0, 1, 2, 3, 4, 5, 9, 3, 2, 1, 1, 2, 4, 5, 6, 7, 8, 9, 0, 2, 1, 1, 1};
         int[] weight = new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52};
         int W = 345;
-        // TestCase.assertEquals(36, findBestValueWithoutMemonization(value, weight, W));
+        // TestCase.assertEquals(119, findBestValueWithoutMemonization(value, weight, W));
         TestCase.assertEquals(119, findBestValueWithMatrix(value, weight, W));
     }
 }
---
+++ src/main/java/com/aiyyatti/algorithms/misc/recursionanddynamic/recursionanddynamicprogrammic/Coins.java
@@ -0,0 +1,37 @@
+package com.aiyyatti.algorithms.misc.recursionanddynamic.recursionanddynamicprogrammic;
+
+import junit.framework.TestCase;
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Coins: Given an innnite number of quarters (25 cents), dimes (10 cents), nickels (5 cents), and
+ * pennies (1 cent), write code to calculate the number of ways of representing n cents.
+ */
+public class Coins {
+    Logger logger = LoggerFactory.getLogger(Coins.class);
+    private static final int[] COINS = new int[]{25, 10, 5, 1};
+
+    @Test
+    public void simpleTest1() {
+        TestCase.assertEquals(4, numberOfWaysOfRepresending(10));
+    }
+
+    private int numberOfWaysOfRepresending(int N) {
+        return numberOfWaysOfRepresending(0, N, "");
+    }
+
+    private int numberOfWaysOfRepresending(int index, int remainingAmount, String str) {
+        if (index > COINS.length || remainingAmount < 0) return 0;
+        if (remainingAmount == 0) {
+            logger.info("{}", str);
+            return 1;
+        }
+        int numOfWays = 0;
+        for (int i = index; i < COINS.length; i++) {
+            numOfWays += numberOfWaysOfRepresending(i, remainingAmount - COINS[i], str + " " + COINS[i]);
+        }
+        return numOfWays;
+    }
+}
