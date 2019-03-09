b96367d350c2fd3aa3e893e40948033d14e6714b
redocyzal <achuth.i@gmail.com>
2019-03-09 19:00:18 +1100

    algorithms - inorder traversal

--- src/main/java/com/aiyyatti/algorithms/misc/tree/InOrderTraversalWithoutRecustion.java
+++ src/main/java/com/aiyyatti/algorithms/misc/tree/InOrderTraversalWithoutRecustion.java
@@ -13,14 +13,28 @@ public class InOrderTraversalWithoutRecustion {
      * Follow the below rules:
      * 0) add the root node into the stack
      * 1) until stack is empty - peek the top of the node, if the node has a left node
-     *    keep adding it and left child to a stack until it hits a null.
+     * keep adding it and left child to a stack until it hits a null.
      * 2) when it encounters a null pop until the next non null element and the print it
-     *    then add the right node and repeat (1).
+     * then add the right node and repeat (1).
+     *
      * @return
      */
-    private String doInOrderTraversalWithoutRecursion() {
+    private String doInOrderTraversalWithoutRecursion(Node root) {
         Stack<Node> stack = new Stack<>();
-        return null;
+        stack.push(root);
+        StringBuilder output = new StringBuilder("");
+        while (!stack.isEmpty()) {
+            Node node = stack.peek();
+            if (node == null) {
+                stack.pop();
+                if (!stack.isEmpty()) {
+                    Node prevToThat = stack.pop();
+                    output.append(prevToThat + " ");
+                    stack.push(prevToThat.right);
+                }
+            } else stack.push(node.left);
+        }
+        return output.toString();
     }
 
     ///////////////
@@ -38,13 +52,18 @@ public class InOrderTraversalWithoutRecustion {
         Node n7 = new Node(7);
         n0.left(n1);
         n0.right(n5);
-        n1.left(n1);
+        n1.left(n2);
         n1.right(n3);
         n3.right(n4);
         n0.right(n5);
         n5.left(n6);
         n5.right(n7);
-        TestCase.assertEquals("2 1 4 3 0 6 5 7", doInOrderTraversalWithoutRecursion());
+        TestCase.assertEquals(inOrder(n0), doInOrderTraversalWithoutRecursion(n0));
+    }
+
+    public String inOrder(Node root) {
+        if (root == null) return "";
+        return inOrder(root.left()) + root.data + " " + inOrder(root.right());
     }
 
     ////////////////////
@@ -71,7 +90,7 @@ public class InOrderTraversalWithoutRecustion {
             this.left = left;
         }
 
-        public void right(Node left) {
+        public void right(Node right) {
             this.right = right;
         }
 
