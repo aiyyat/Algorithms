137b7e3803a3beb3d16e43cf7f7456e071deb702
redocyzal <achuth.i@gmail.com>
2019-04-02 07:16:46 +1100

    algorithms: ctci

---
+++ src/main/java/com/aiyyatti/algorithms/ctci/bigo/Prime.java
@@ -0,0 +1,32 @@
+package com.aiyyatti.algorithms.ctci.bigo;
+
+import junit.framework.TestCase;
+import org.junit.Test;
+
+public class Prime {
+    @Test
+    public void simpleTest() {
+        TestCase.assertTrue(isPrime(307));
+    }
+
+    @Test
+    public void simpleTest2() {
+        TestCase.assertFalse(isPrime(308));
+    }
+
+    @Test
+    public void simpleTest3() {
+        TestCase.assertTrue(isPrime(1223));
+    }
+
+    @Test
+    public void simpleTest4() {
+        TestCase.assertTrue(isPrime(2699));
+    }
+
+    public boolean isPrime(int n) {
+        if (n % 2 == 0) return false;
+        for (int i = 3; i * i < n; i++) if (n % i == 0) return false;
+        return true;
+    }
+}
