a2cc484fc64fee84a9b4fa276ddc8262287942b2
aiyyatti <achuth.i@gmail.com>
2019-04-15 16:07:53 +1000

    Create AddWithoutPlus.java

---
+++ src/main/java/com/aiyyatti/algorithms/ctci/hard/AddWithoutPlus.java
@@ -0,0 +1,24 @@
+package com.aiyyatti.algorithms.ctci.linkedlist;
+
+import org.junit.Test;
+
+public class AddWithoutPlus {
+    @Test
+    public void ctciTest() {
+        System.out.println(doAddWithoutPlus(759, 674));
+    }
+
+    public int doAddWithoutPlus(int num1, int num2) {
+        // num1 ^ num2 - sum without carry
+        // num1 & numb2 - carry whhout sum
+        int carry = 0;
+        int sumWithoutCarry = 0;
+        do {
+            sumWithoutCarry = num1 ^ num2;
+            carry = num1 & num2;
+            num1 = sumWithoutCarry;
+            num2 = carry << 1;
+        } while (carry != 0);
+        return sumWithoutCarry;
+    }
+}
