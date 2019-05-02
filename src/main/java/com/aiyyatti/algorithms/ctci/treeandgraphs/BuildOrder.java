d5c15dd1498bbd58e747e84627f6dc3edb30c98e
redocyzal <achuth.i@gmail.com>
2019-05-03 06:06:02 +1000

    algorithms: misc commits

--- src/main/java/com/aiyyatti/algorithms/ctci/treeandgraphs/BuildOrder.java
+++ src/main/java/com/aiyyatti/algorithms/ctci/treeandgraphs/BuildOrder.java
@@ -1,10 +1,16 @@
 package com.aiyyatti.algorithms.ctci.treeandgraphs;
 
 import junit.framework.TestCase;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 
 import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.stream.Collectors;
+
+import static junit.framework.TestCase.assertEquals;
 
 /**
  * <b>Build Order:</b> You are given a list of projects and a list of dependencies (which is a list of pairs of
@@ -24,56 +30,26 @@ public class BuildOrder {
     //////////////
     // SOLUTION //
     //////////////
-    enum Status {
-        NOT_BUILT, BUILT
-    }
-
-    class BuildTool {
-        Project[] projects = null;
-        int offset = 0;
-        Project[] buildOrder = null;
-
-        public BuildTool(Project... projects) {
-            this.projects = projects;
-            buildOrder = new Project[projects.length];
-        }
-
-        public Project[] doBuild() {
-            while (offset < projects.length) {
-                for (int i = 0; i < projects.length; i++) {
-                    Project project = projects[i];
-                    if (project.status == Status.NOT_BUILT && project.areAllDependentProjectsBuilt()) {
-                        buildOrder[offset++] = project;
-                        project.status = Status.BUILT;
-                    }
-                }
-            }
-            return buildOrder;
-        }
-    }
-
     class Project {
-        private String name;
-        private Status status = Status.NOT_BUILT;
-        private Project[] dependencies = null;
+        String name;
+        LinkedList<Project> children = new LinkedList<>();
+        int parentDependencyCount = 0;
 
         public Project(String name) {
             this.name = name;
         }
 
-        public Project(String name, Project... dependencies) {
-            this.name = name;
-            this.dependencies = dependencies;
+        public int incrementParentDependencyCount() {
+            return parentDependencyCount++;
         }
 
-        public boolean areAllDependentProjectsBuilt() {
-            if (dependencies == null) return true;
-            for (int i = 0; i < dependencies.length; i++) {
-                if (dependencies[i].status == Status.NOT_BUILT) {
-                    return false;
-                }
-            }
-            return true;
+        public int decrementParentDependencyCount() {
+            return parentDependencyCount--;
+        }
+
+        public void addChild(Project b) {
+            children.add(b);
+            b.incrementParentDependencyCount();
         }
 
         @Override
@@ -82,19 +58,98 @@ public class BuildOrder {
         }
     }
 
+    class ProjectBuilder {
+        List<Project> projects = new LinkedList<>();
+
+        public ProjectBuilder(List<Project> projects) {
+            this.projects = projects;
+        }
+
+        public Project[] build() {
+            int N = projects.size();
+            Project[] output = new Project[N];
+            int buildTo = 0;
+            int buildFrom = 0;
+            buildTo = zeroDeps(projects, output, buildTo);
+            while (true) {
+                if (buildTo == buildFrom) {
+                    if (buildTo < N) throw new RuntimeException("Cyclic");
+                    else return output;
+                }
+                for (; buildFrom < buildTo; buildFrom++) {
+                    List<Project> children = output[buildFrom].children;
+                    for (Project child : children) {
+                        child.decrementParentDependencyCount();
+                    }
+                    buildTo = zeroDeps(children, output, buildTo);
+                }
+            }
+        }
+
+        public int zeroDeps(List<Project> projects, Project[] output, int buildTo) {
+            for (Project project : projects) {
+                if (project.parentDependencyCount == 0) {
+                    output[buildTo] = project;
+                    buildTo++;
+                }
+            }
+            return buildTo;
+        }
+    }
+
     ////////////////
     // TEST CASES //
     ////////////////
     @Test
-    public void simpleTest() {
-        Project fileWriter = new Project("File Writer");
-        Project consoleWriter = new Project("Console Writer");
-        Project logger = new Project("Logger", fileWriter, consoleWriter);
-        Project appleMaps = new Project("Apple Maps", logger);
-        Project grl = new Project("Global Resource Locator", appleMaps, logger);
-        Project appleDotCom = new Project("appleDotCom", grl);
-        BuildTool buildTool = new BuildTool(consoleWriter, logger, appleDotCom, fileWriter, appleMaps, grl);
-        String output = Arrays.stream(buildTool.doBuild()).map(Project::toString).collect(Collectors.joining("->"));
-        TestCase.assertEquals("Console Writer->File Writer->Logger->Apple Maps->Global Resource Locator->appleDotCom", output);
+    public void ctciTest() {
+        Project a = new Project("a");
+        Project b = new Project("b");
+        Project c = new Project("c");
+        Project d = new Project("d");
+        Project e = new Project("e");
+        Project f = new Project("f");
+        a.addChild(d);
+        f.addChild(b);
+        b.addChild(d);
+        f.addChild(a);
+        d.addChild(c);
+        List<Project> projects = new LinkedList<>();
+        projects.add(a);
+        projects.add(b);
+        projects.add(c);
+        projects.add(d);
+        projects.add(e);
+        projects.add(f);
+        assertEquals("[e, f, b, a, d, c]", Arrays.toString(new ProjectBuilder(projects).build()));
     }
+
+    @Rule
+    public ExpectedException expectedException = ExpectedException.none();
+
+    @Test
+    public void cyclicTest() {
+        expectedException.expect(RuntimeException.class);
+        expectedException.expectMessage("Cyclic");
+        Project a = new Project("a");
+        Project b = new Project("b");
+        Project c = new Project("c");
+        Project d = new Project("d");
+        Project e = new Project("e");
+        Project f = new Project("f");
+        a.addChild(d);
+        c.addChild(a);
+        f.addChild(b);
+        b.addChild(d);
+        f.addChild(a);
+        d.addChild(c);
+        List<Project> projects = new LinkedList<>();
+        projects.add(a);
+        projects.add(b);
+        projects.add(c);
+        projects.add(d);
+        projects.add(e);
+        projects.add(f);
+        new ProjectBuilder(projects).build();
+    }
+
 }
