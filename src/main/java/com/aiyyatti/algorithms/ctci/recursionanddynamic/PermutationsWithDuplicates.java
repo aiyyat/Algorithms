c335bd751701104a58e7d36d2a7968cac5f3b463
redocyzal <achuth.i@gmail.com>
2019-05-04 06:25:49 +1000

    algorithms

--- src/main/java/com/aiyyatti/algorithms/ctci/recursionanddynamic/PermutationsWithDuplicates.java
+++ src/main/java/com/aiyyatti/algorithms/ctci/recursionanddynamic/PermutationsWithDuplicates.java
@@ -11,15 +11,9 @@ public class PermutationsWithDuplicates {
     //////////////
     @Test
     public void simpleTest() {
-        doPermutationsWithDuplicates("abc", 0);
+        doPermutationsWithDuplicates("aabcccc", 0);
     }
 
     public void doPermutationsWithDuplicates(String str, int index) {
-        int N = str.length();
-        if (index == N) System.out.println(str);
-        for (int i = index; i < N; i++) {
-            char ith = str.charAt(i);
-            doPermutationsWithDuplicates(str.substring(0, index) + ith + str.substring(index, i) + str.substring(i + 1), index + 1);
-        }
     }
 }
--- src/main/java/com/aiyyatti/algorithms/ctci/recursionanddynamic/PermutationsWithoutDups.java
+++ src/main/java/com/aiyyatti/algorithms/ctci/recursionanddynamic/PermutationsWithoutDups.java
@@ -13,32 +13,6 @@ public class PermutationsWithoutDups {
     //TEST CASE//
     ////////////
     @Test
-    public void simpleNonCtciApproachTest1() {
-        String str = "abc";
-        TestCase.assertEquals(factorial(str), doPermutationsWithoutDupsNonCtci(str).size());
-    }
-
-    @Test
-    public void simpleNonCtciApproachTest3() {
-        String str = "ab";
-        ArrayList<String> output = doPermutationsWithoutDupsNonCtci(str);
-        System.out.println(output);
-        TestCase.assertEquals(factorial(str), output.size());
-    }
-
-    @Test
-    public void simpleNonCtciApproachTest4() {
-        String str = "a";
-        TestCase.assertEquals(factorial(str), doPermutationsWithoutDupsNonCtci(str).size());
-    }
-
-    @Test
-    public void simpleNonCtciApproachTest2() {
-        String str = "abcdefgh";
-        TestCase.assertEquals(factorial(str), doPermutationsWithoutDupsNonCtci(str).size());
-    }
-
-    @Test
     public void simpleCtciApproachTest1() {
         String str = "abc";
         TestCase.assertEquals(factorial(str), doPermutationsWithoutDupsCtci(str).size());
@@ -71,15 +45,12 @@ public class PermutationsWithoutDups {
         else return a * factorial(a - 1);
     }
 
-    public ArrayList<String> doPermutationsWithoutDupsNonCtci(String str) {
-        return doPermutationsWithoutDupsNonCtci(str, 0);
-    }
-
     public ArrayList<String> doPermutationsWithoutDupsCtci(String str) {
-        return doPermutationsWithoutDupsNonCtci(str, 0);
+        return doPermutationsWithoutDupsCtci(str, 0);
     }
 
     /**
+     * Always making the mistake of not going till the end to find the unique value to be added to the arraylist.
      * Time Complexity: O(N!)
      *
      * @param str
@@ -87,41 +58,17 @@ public class PermutationsWithoutDups {
      * @return
      */
     public ArrayList<String> doPermutationsWithoutDupsCtci(String str, int index) {
-        ArrayList<String> output = new ArrayList<>();
+        ArrayList<String> output = new ArrayList<String>();
         int N = str.length();
-        if (index == N) return output;
-        for (int i = 0; i < N; i++) {
-            char ch = str.charAt(i);
-            ArrayList<String> intermediateOutputs = doPermutationsWithoutDupsCtci(str, index + 1);
-            for (String intermediateOutput : intermediateOutputs) {
-                for (int j = 0; j < intermediateOutput.length(); i++) {
-                    output.add(intermediateOutput.substring(0, j) + ch + intermediateOutput.substring(j + 1));
-                }
-            }
-        }
-        return output;
-    }
-
-    /**
-     * TODO: Always making the mistake of not going till the end to find the unique value to be added to the arraylist.
-     * TimeComplexity: O(N!)
-     *
-     * @param str
-     * @param index
-     * @return
-     */
-    public ArrayList<String> doPermutationsWithoutDupsNonCtci(String str, int index) {
-        ArrayList<String> output = new ArrayList<>();
-        if (index == str.length() - 1) {
+        if (index == N) {
             output.add(str);
             return output;
         }
-        int N = str.length();
         for (int i = index; i < N; i++) {
-            output.addAll(doPermutationsWithoutDupsNonCtci(str, index + 1));
-            str = str.substring(0, index) + str.substring(index + 1) + str.charAt(index);
+            char ith = str.charAt(i);
+            // or substring of 0 to i and put ith in the end?
+            output.addAll(doPermutationsWithoutDupsCtci(str.substring(0, index) + ith + str.substring(index, i) + str.substring(i + 1), index + 1));
         }
         return output;
     }
-
 }
