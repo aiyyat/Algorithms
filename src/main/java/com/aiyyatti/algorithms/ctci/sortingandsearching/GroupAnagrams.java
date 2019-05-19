a33cc995f5294ce6fe1b4e159aa6218cefaf3c66
redocyzal <achuth.i@gmail.com>
2019-05-19 21:01:04 +1000

    algorithms: script -> auto-commit & push

--- src/main/java/com/aiyyatti/algorithms/ctci/sortingandsearching/GroupAnagrams.java
+++ src/main/java/com/aiyyatti/algorithms/ctci/sortingandsearching/GroupAnagrams.java
@@ -1,8 +1,42 @@
 package com.aiyyatti.algorithms.ctci.sortingandsearching;
 
-/**
- * approach 1: sort it and used sorted as key and original as value O(N*log(M))
- * approach 2: a1b2c5 => as key - would be faster. O(N*M)
- */
+import junit.framework.TestCase;
+import org.junit.Test;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+
+import static junit.framework.TestCase.assertEquals;
+
 public class GroupAnagrams {
+    @Test
+    public void simpleTest() {
+        String[] strings = {"army", "mary", "mate", "team", "bend", "said", "note", "tone"};
+        assertEquals("[said, note, tone, bend, mate, team, army, mary]", Arrays.toString(doGroupAnagram(strings)));
+    }
+
+    public String[] doGroupAnagram(String[] words) {
+        HashMap<String, ArrayList<String>> table = new HashMap<>();
+        for (String word : words) {
+            String charSort = charSort(word);
+            ArrayList<String> group = table.getOrDefault(charSort, new ArrayList<>());
+            group.add(word);
+            table.put(charSort, group);
+        }
+        int offset = 0;
+        for (String key : table.keySet()) {
+            for (String word : table.get(key)) {
+                words[offset++] = word;
+            }
+        }
+        return words;
+    }
+
+    public String charSort(String str) {
+        char[] c = str.toCharArray();
+        Arrays.sort(c);
+        return new String(c);
+    }
 }
