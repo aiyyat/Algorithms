ce882d6d9c8bceafdb14714923bcd4ee1daba1c5
redocyzal <achuth.i@gmail.com>
2019-05-19 20:26:13 +1000

    algorithms: script -> auto-commit & push

--- src/main/java/com/aiyyatti/algorithms/ctci/sortingandsearching/SortedMerge.java
+++ src/main/java/com/aiyyatti/algorithms/ctci/sortingandsearching/SortedMerge.java
@@ -10,40 +10,38 @@ import static junit.framework.TestCase.assertEquals;
 public class SortedMerge {
     @Test
     public void simpleTest() {
-        int[] a = new int[]{1, 5, 7, 9};
+        int[] a = new int[]{1, 5, 7, 9, -1, -1, -1, -1, -1, -1};
         int[] b = new int[]{2, 3, 4, 6, 8, 10};
         assertEquals("[1, 2, 3, 4, 5, 6, 7, 8, 9, 10]", Arrays.toString(doSortedMerge(a, b)));
     }
 
     @Test
     public void simple2Test() {
-        int[] a = new int[]{1, 2, 3, 4};
+        int[] a = new int[]{1, 2, 3, 4, -1, -1, -1, -1, -1, -1};
         int[] b = new int[]{5, 6, 7, 8, 9, 10};
         assertEquals("[1, 2, 3, 4, 5, 6, 7, 8, 9, 10]", Arrays.toString(doSortedMerge(a, b)));
     }
+
     @Test
     public void simple3Test() {
+        int[] a = new int[]{5, 6, 7, 8, 9, 10, -1, -1, -1, -1};
         int[] b = new int[]{1, 2, 3, 4};
-        int[] a = new int[]{5, 6, 7, 8, 9, 10};
         assertEquals("[1, 2, 3, 4, 5, 6, 7, 8, 9, 10]", Arrays.toString(doSortedMerge(a, b)));
     }
 
     public int[] doSortedMerge(int[] a, int[] b) {
-        int bN = b.length;
-        int aN = a.length;
-        int[] o = new int[aN + bN];
-        int aptr = 0;
-        int bptr = 0;
-        int offset = 0;
+        int bN = b.length - 1;
+        int aOffset = a.length - 1;
+        int aN = aOffset - bN - 1;
+        int aptr = aN;
+        int bptr = bN;
         while (true) {
-            if (aptr >= aN) {
-                System.arraycopy(b, bptr, o, offset, bN - bptr);
-                return o;
-            } else if (bptr >= bN) {
-                System.arraycopy(a, aptr, o, offset, aN - aptr);
-                return o;
-            } else if (a[aptr] > b[bptr]) o[offset++] = b[bptr++];
-            else o[offset++] = a[aptr++];
+            if (aptr < 0 && bptr < 0) break;
+            else if (aptr < 0) while (bptr >= 0) a[aOffset--] = b[bptr--];
+            else if (bptr < 0) while (aptr >= 0) a[aOffset--] = a[aptr--];
+            else if (a[aptr] > b[bptr]) a[aOffset--] = a[aptr--];
+            else a[aOffset--] = b[bptr--];
         }
+        return a;
     }
 }
