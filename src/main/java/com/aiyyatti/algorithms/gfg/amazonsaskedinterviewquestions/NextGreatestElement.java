95a6b7c8663bc2fc455894066f8c28e9758d4165
redocyzal <achuth.i@gmail.com>
2019-05-26 07:38:53 +1000

    algorithms: script -> auto-commit & push

---
+++ src/main/java/com/aiyyatti/algorithms/gfg/amazonsaskedinterviewquestions/NextGreatestElement.java
@@ -0,0 +1,7 @@
+package com.aiyyatti.algorithms.gfg.amazonsaskedinterviewquestions;
+
+/**
+ * https://www.geeksforgeeks.org/next-greater-element/
+ */
+public class NextGreatestElement {
+}
---
+++ src/main/java/com/aiyyatti/algorithms/gfg/amazonsaskedinterviewquestions/StackSpanProblem.java
@@ -0,0 +1,7 @@
+package com.aiyyatti.algorithms.gfg.amazonsaskedinterviewquestions;
+
+/**
+ * https://www.geeksforgeeks.org/the-stock-span-problem/
+ */
+public class StackSpanProblem {
+}
