febbd9e95afaf9c8e7fbb0882487ec54830ede26
redocyzal <achuth.i@gmail.com>
2019-06-03 07:43:09 +1000

    algorithms: script -> auto-commit & push

--- src/main/java/com/aiyyatti/algorithms/leetcode/BestTimeToBuyAndSellStockWithTransactionFee.java
+++ src/main/java/com/aiyyatti/algorithms/leetcode/BestTimeToBuyAndSellStockWithTransactionFee.java
@@ -14,10 +14,30 @@ public class BestTimeToBuyAndSellStockWithTransactionFee {
     public void simpleTest() {
         int[] a = new int[]{1, 3, 2, 8, 4, 9};
         int fee = 2;
-        TestCase.assertEquals(8, doBestTimeToBuyAndSellStockWithTransactionFee(a, fee));
+        TestCase.assertEquals(8, ownNotOwnApproach(a, fee));
     }
 
-    public int doBestTimeToBuyAndSellStockWithTransactionFee(int[] a, int fee) {
-        return -1;
+    public int ownNotOwnApproach(int[] a, int fee) {
+        int own = -1 * a[0];
+        int notOwn = 0;
+        int N = a.length;
+        for (int i = 1; i < N; i++) {
+            int tempOwn = Math.max(notOwn - a[i], own);
+            int tempNotOwn = Math.max(own + a[i] - fee, notOwn);
+            own = tempOwn;
+            notOwn = tempNotOwn;
+        }
+        return Math.max(own, notOwn);
+    }
+
+    public int peakValleyApproach(int[] a, int fee) {
+        int N = a.length;
+        int boughtAt = a[0];
+        int balance = 0;
+        for(int i=1;i<N;i++){
+            if(a[i]>a[i+1]) {
+                balance+=boughtAt-a[i];
+            }
+        }
     }
 }
