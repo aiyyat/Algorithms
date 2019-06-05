8e8a86df54aed76467b8546c93ce159d21a2ed78
redocyzal <achuth.i@gmail.com>
2019-06-06 06:54:17 +1000

    algorithms: script -> auto-commit & push

--- src/main/java/com/aiyyatti/algorithms/ctci/hard/MissingNumber.java
+++ src/main/java/com/aiyyatti/algorithms/ctci/hard/MissingNumber.java
@@ -3,12 +3,14 @@ package com.aiyyatti.algorithms.ctci.hard;
 import junit.framework.TestCase;
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 import static junit.framework.TestCase.assertEquals;
 
 /**
- * TODO Must Practise
- * Working but with a lot of code. Understand from CTCI how that works.
- * This approach:
+ * Brute Force:
  * 1) calculate the number of complete blocks from 0 for both numbers
  * 2) if the block count is odd then there is one extra block to be considered
  * e.g. if 1 complete block result must be 1*p below.
@@ -22,22 +24,68 @@ public class MissingNumber {
     // TEST CASES //
     ////////////////
     @Test
-    public void simpleTest() {
-        int[] a = new int[]{3, 4, 5, 7, 8};
-        assertEquals(6, findMissingNumber(a, 8, 3));
+    public void simple1Test() {
+        int N = 7;
+        int[] a = new int[N - 1];
+        for (int i = 0; i < N; i++) {
+            int offset = 0;
+            for (int j = 0; j < N; j++) {
+                if (j != i) {
+                    a[offset++] = j;
+                }
+            }
+            TestCase.assertEquals(i, findMissingNumber(a, N));
+            TestCase.assertEquals(i, findMissingNumberBruteForce(a, N));
+        }
+    }
+
+    @Test
+    public void simple2Test() {
+        int[] a = new int[]{3, 4, 5, 7, 0, 2, 1};
+        assertEquals(6, findMissingNumber(a, 7));
+        assertEquals(6, findMissingNumberBruteForce(a, 8, 3));
     }
 
     @Test
     public void testFindMissingNumber() {
-        findMissingNumber(null, 11, 2);
+        findMissingNumberBruteForce(null, 11, 2);
     }
 
-    @Test
-    public void specifictestFindMissingNumber() {
-        System.out.println(zerosInRange(5, 0));
+    //////////
+    // CTCI //
+    //////////
+    public int findMissingNumber(int[] a, int end) {
+        int M = Integer.toBinaryString(end).length();
+        return doFindMissingNumber(a, M, 0);
     }
 
-    public int findMissingNumber(int[] a, int end, int start) {
+    public int doFindMissingNumber(int[] a, int M, int current) {
+        if (current == M) return 0;
+        int N = a.length;
+        List<Integer> zeros = new ArrayList<>();
+        List<Integer> ones = new ArrayList<>();
+        for (int j = 0; j < N; j++) {
+            if ((a[j] & 1 << (current)) == 0) zeros.add(a[j]);
+            else ones.add(a[j]);
+        }
+        int missing = 1;
+        if (zeros.size() <= ones.size()) {
+            missing = 0;
+            return (doFindMissingNumber(zeros.stream().mapToInt(Integer::intValue).toArray(), M, current + 1)) << 1;
+        } else {
+            missing = 1;
+            return (doFindMissingNumber(ones.stream().mapToInt(Integer::intValue).toArray(), M, current + 1)) << 1 | 1;
+        }
+    }
+
+    /////////////////
+    // BRUTE FORCE //
+    /////////////////
+    public int findMissingNumberBruteForce(int[] a, int end) {
+        return findMissingNumberBruteForce(a, end, 0);
+    }
+
+    public int findMissingNumberBruteForce(int[] a, int end, int start) {
         int digits = 0;
         String output = "";
         while (Math.pow(2, digits) <= end) {
